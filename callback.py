f27efd41d6a2e7ea323c029a2e089b74237f78f0
Kai Katsumata <imperialroyalguard38@gmail.com>
2017-01-20 08:19:08 +0900

    typo

--- callback.py
+++ callback.py
@@ -2,12 +2,12 @@ import falcon
 import json
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMssage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 REPLY_ENDPOINT = 'https://api.line.me/v2/bot/message/reply'
 ACCESS_TOKEN = '7pp1e5yQqTj7AEWAJIzk/B9pa1WDqJJbUCbwbCH4cdSI/A1RDST5RfAxw9TfZD9OPYo4vNTkfWZyELrXD9TRMpMqN60pohkbwpQM0e55i33Ycd3EGcxVGSKA+YhrV7OcMfoeHHhAawSDjAUnkhTA0QdB04t89/1O/w1cDnyilFU='
 CHANNEL_SECRET = '973f214ae9a2cc10b4497e80201973b9'
-line_bot_api = LineBotApi(ACCES_TOKEN)
+line_bot_api = LineBotApi(ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 
 class Resource(object):
@@ -26,7 +26,7 @@ class Resource(object):
         except InvalidSignatureError:
             abort(400)
 
-@handler.add(MessageEvent, mesage=TextMessage)
+@handler.add(MessageEvent, message=TextMessage)
 def handle_mesage(event):
     line_bot_api.reply_message(event['events']['replyToken'], TextSendMessage(text=event['events'][0]['message']['text']))
                                
