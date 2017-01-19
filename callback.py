68ae4447700fb9d73a9b3e2d012b555848244987
Kai Katsumata <imperialroyalguard38@gmail.com>
2017-01-20 08:07:10 +0900

    x

--- callback.py
+++ callback.py
@@ -7,7 +7,7 @@ from linebot.models import MessageEvent, TextMessage, TextSendMssage
 REPLY_ENDPOINT = 'https://api.line.me/v2/bot/message/reply'
 ACCESS_TOKEN = '7pp1e5yQqTj7AEWAJIzk/B9pa1WDqJJbUCbwbCH4cdSI/A1RDST5RfAxw9TfZD9OPYo4vNTkfWZyELrXD9TRMpMqN60pohkbwpQM0e55i33Ycd3EGcxVGSKA+YhrV7OcMfoeHHhAawSDjAUnkhTA0QdB04t89/1O/w1cDnyilFU='
 CHANNEL_SECRET = '973f214ae9a2cc10b4497e80201973b9'
-line_bot_api = LineBotApi(REPLY_ENDPOINT)
+line_bot_api = LineBotApi(ACCES_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 
 class Resource(object):
