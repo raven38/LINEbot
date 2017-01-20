8afbcf725eb03c696b8ba8cf1dc0a3d5dbe9aeae
raven38 <imperialroyalguard38@gmail.com>
2017-01-20 15:51:50 +0900

    change

--- callback.py
+++ callback.py
@@ -4,14 +4,14 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
-ACCESS_TOKEN = 'zQSxDZOVNQ6P6xHaDmuM5WZqdoREQUNknDqBscHBFzNYLzk/qvPm2G0MniAA3FyLM3aLIbZB2Tn2jKHarFasmhFGICYVOVdhZPhNtq3mybPS3cTFXgP6hKGUgKx/pONldRPt7cCJQuJWPl6UsYKthQdB04t89/1O/w1cDnyilFU='
-CHANNEL_SECRET = 'dd1e8da2217482d939cbda0fb4b52b50'
+ACCESS_TOKEN = '7pp1e5yQqTj7AEWAJIzk/B9pa1WDqJJbUCbwbCH4cdSI/A1RDST5RfAxw9TfZD9OPYo4vNTkfWZyELrXD9TRMpMqN60pohkbwpQM0e55i33Ycd3EGcxVGSKA+YhrV7OcMfoeHHhAawSDjAUnkhTA0QdB04t89/1O/w1cDnyilFU='
+CHANNEL_SECRET = '973f214ae9a2cc10b4497e80201973b9'
 line_bot_api = LineBotApi(ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 
 class Resource(object):
     def on_post(self, req, resp):
-        print(req.headers)
+#        print(req.headers)
         signature = req.headers['X-LINE-SIGNATURE']
         body = req.stream.read()
         body = body.decode('utf-8')
@@ -20,10 +20,11 @@ class Resource(object):
         except InvalidSignatureError:
             abort(400)
 
-
 @handler.add(MessageEvent, message=TextMessage)
-def handle_mesage(event):
-    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
+def handle_message(event):
+#    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
+    print(type(event.message.text))
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text='こんにちは'))
                                
 app = falcon.API()
 app.add_route('/callback', Resource())
